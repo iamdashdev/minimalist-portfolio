d439bf3ea22d1ecfeec3b9b5ac3f7589a4178651
Polly Mwangi <iamdashdeveloper@gmail.com>
2025-07-09 20:05:23 +0300

    Refactor skills data structure for improved readability and consistency

--- frontend/src/assets/skills.js
+++ frontend/src/assets/skills.js
@@ -1,40 +1,44 @@
 const skills = [
-    {
-        cartegory: "Tools",
-        name: [
-            "Arcgis Pro", "Qgis", "Figma", "VSCode", "Erdas Imagine", "Envi", "Google Earth Engine",
-            "Excel", "Powerpoint", "Word"
-        ]
-
-    },
-    {
-        cartegory: "Languages",
-        name: [
-            "Python", "JavaScript", "SQL", "C++", "Java", "PHP"
-        ]
-    },
-     {
-        cartegory: "Databases",
-        name: [
-            "PostgreSQL", "MongoDB", "SQLite"
-        ]
-    },
-     {
-        cartegory: "Frameworks",
-        name: [
-            "React", "Vue", "Express", "Node", "Django", "Flask", "Leaflet.js", "Tailwind"
-        ]
-    },
-     {
-        cartegory: "Others",
-        name: [
-            "HTML", "CSS", "Hubspot", "Zapier", "Wordpress", "Kepler"
-        ]
-    },
-    
-    
-    
-
-]
+	{
+		cartegory: "Tools",
+		name: [
+			"Arcgis Pro",
+			"Qgis",
+			"Figma",
+			"VSCode",
+			"Erdas Imagine",
+			"Envi",
+			"Google Earth Engine",
+			"Excel",
+			"Powerpoint",
+			"Word"
+		]
+	},
+	{
+		cartegory: "Languages",
+		name: ["Python", "JavaScript", "SQL", "C++", "C#"]
+	},
+	{
+		cartegory: "Databases",
+		name: ["PostgreSQL", "MongoDB", "SQLite"]
+	},
+	{
+		cartegory: "Frameworks",
+		name: [
+			"React",
+			"Vue",
+			"Express",
+			"Node",
+			"Django",
+			"Flask",
+			"Leaflet.js",
+			"Tailwind"
+		]
+	},
+	{
+		cartegory: "Others",
+		name: ["HTML", "CSS", "Hubspot", "Zapier", "Wordpress", "Kepler"]
+	}
+];
 
 export default skills;
